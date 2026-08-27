665ffcd243aaa3a69ea67a37dca7a2e14d86084a
Izzy <8mrl1002@gmail.com>
2026-08-27 08:20:53 -0600

    Added ability to edit todo items

--- ToDo.Client/ToDo.Client.Client/bin/Debug/net10.0/wwwroot/_framework/dotnet.js
+++ ToDo.Client/ToDo.Client.Client/bin/Debug/net10.0/wwwroot/_framework/dotnet.js
@@ -5,7 +5,7 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "mainAssemblyName": "ToDo.Client.Client",
   "applicationEnvironment": "Development",
   "resources": {
-    "hash": "sha256-2XEXFKI88JWatankQCEcaNtyVGjrJ93JP8ifpxNIEAk=",
+    "hash": "sha256-NDxWIgJ9WDyCwOJStbXzGdtdL8C9+tc954xg3vzd+dY=",
     "jsModuleNative": [
       {
         "name": "dotnet.native.vzj2a6aakt.js"
@@ -1242,16 +1242,16 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       },
       {
         "virtualPath": "ToDo.Client.Client.wasm",
-        "name": "ToDo.Client.Client.j14ira3rtg.wasm",
-        "hash": "sha256-9zdWz2sdVm4KniQTjAolFZNC/T5zRUFYxpksJ59rO6I=",
+        "name": "ToDo.Client.Client.8udycqkyuy.wasm",
+        "hash": "sha256-cJF3Nwz9UgHzc+hj/ROHcbM9DNbO2histqZ2rxx23v0=",
         "cache": "force-cache"
       }
     ],
     "pdb": [
       {
         "virtualPath": "ToDo.Client.Client.pdb",
-        "name": "ToDo.Client.Client.fln84f4ga9.pdb",
-        "hash": "sha256-c4n6vegvqo56mUYnLk8/0cYy71m2NHyVKUr6RT8bL+4=",
+        "name": "ToDo.Client.Client.hqvwq3ucyq.pdb",
+        "hash": "sha256-qY5qocGP785TSEv/lkYMRFSkOy0OpRFUqTZzXAdHYg8=",
         "cache": "force-cache"
       }
     ],
--- ToDo.Client/ToDo.Client/obj/Debug/net10.0/ToDo.Client.AssemblyInfo.cs
+++ ToDo.Client/ToDo.Client/obj/Debug/net10.0/ToDo.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87814818a9e0233635ee452f912530c0c677a58d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7557b5ffc06b3728f7526149fdf0f9fd124fb960")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
